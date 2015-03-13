0659d2a8a1176ef1a7b8add393a923bff85aca70
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-13 14:08:20 +0000

    Handle variable, binary op, call exprs in AST
    Right now we only create the AST, so there are no useful accessor
    methods on them. It would be very easy to add a virtual method to pretty
    print the code, for example. Here are the other expression AST node
    definitions that we’ll use in the basic form of the Kaleidoscope
    language:
    
    [code]
    
    This is all (intentionally) rather straight-forward: variables capture
    the variable name, binary operators capture their opcode (e.g. ‘+’), and
    calls capture a function name as well as a list of any argument
    expressions. One thing that is nice about our AST is that it captures
    the language features without talking about the syntax of the language.
    Note that there is no discussion about precedence of binary operators,
    lexical structure, etc.

--- Lexer.cpp
+++ Lexer.cpp
@@ -71,3 +71,28 @@ class NumberExprAST : public ExprAST {
 public:
     NumberExprAST(double val) : Val(val) {}
 };
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+  std::string Name;
+public:
+  VariableExprAST(const std::string &name) : Name(name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  ExprAST *LHS, *RHS;
+public:
+  BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
+    : Op(op), LHS(lhs), RHS(rhs) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<ExprAST*> Args;
+public:
+  CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+    : Callee(callee), Args(args) {}
+};
