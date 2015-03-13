94b65d0a7814f2b3b310dda0242f0a597683e9b9
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-13 21:27:00 +0000

    Add include directives; fix small formatting
    Add a space before the start of text of comment.

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,11 +1,26 @@
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <string>
+#include <vector>
+
+using std::string;
+
+//===----------------------------------------------------------------------===//
+// Lexer
+//===----------------------------------------------------------------------===//
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
 enum class Token {
     tok_eof = -1,
-    
+
     // commands
     tok_def = -2, tok_extern = -3,
 
-    //primary
-    tok_identifier = -4, tok_number = -5,
+    // primary
+    tok_identifier = -4, tok_number = -5
 };
 
 static string IdentifierStr;  // Filled in if tok_identifier
