6b04f85e61e2b7e66293bb7fb1a853907ce16ad5
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-13 12:24:41 +0000

    Start implementation of AST; handle number exprs
    The AST for a program captures its behavior in such a way that it is
    easy for later stages of the compiler (e.g. code generation) to
    interpret. We basically want one object for each construct in the
    language, and the AST should closely model the language. In
    Kaleidoscope, we have expressions, a prototype, and a function object.
    We’ll start with expressions first:
    
    [code]
    
    The code above shows the definition of the base ExprAST class and one
    subclass which we use for numeric literals. The important thing to note
    about this code is that the NumberExprAST class captures the numeric
    value of the literal as an instance variable. This allows later phases
    of the compiler to know what the stored numeric value is.

--- Lexer.cpp
+++ Lexer.cpp
@@ -59,3 +59,15 @@ static int gettok() {
     return ThisChar;
 }
 
+/// ExprAST - Base class for all expression nodes.
+class ExprAST {
+public:
+    virtual ~ExprAST() {}
+};
+
+/// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+    double Val;
+public:
+    NumberExprAST(double val) : Val(val) {}
+};
