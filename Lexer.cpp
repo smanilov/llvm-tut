b9c7fd55a101568a09291ef0e6263fe5f1fddf90
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-13 17:27:50 +0000

    Add primary expression switch
    Now that we have all of our simple expression-parsing logic in place, we
    can define a helper function to wrap it together into one entry point.
    We call this class of expressions “primary” expressions, for reasons
    that will become more clear later in the tutorial. In order to parse an
    arbitrary primary expression, we need to determine what sort of
    expression it is:
    
    [code]
    
    Now that you see the definition of this function, it is more obvious why
    we can assume the state of CurTok in the various functions. This uses
    look-ahead to determine which sort of expression is being inspected, and
    then parses it with a function call.

--- Lexer.cpp
+++ Lexer.cpp
@@ -182,3 +182,16 @@ static ExprAST *ParseIdentifierExpr() {
 
     return new CallExprAST(IdName, Args);
 }
+
+/// primary
+///   ::= identifierexpr
+///   ::= numberexpr
+///   ::= parenexpr
+static ExprAST *ParsePrimary() {
+    switch (CurTok) {
+    default: return Error("unknown token when expecting an expression");
+    case tok_identifier: return ParseIdentifierExpr();
+    case tok_number:     return ParseNumberExpr();
+    case '(':            return ParseParenExpr();
+    }
+}
