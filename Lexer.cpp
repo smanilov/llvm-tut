1c535befb1b1d3648f6cc11603e32e932b72b7d2
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-13 21:34:19 +0000

    Make Parser code local scope; move it around
    The parser code is thus local/private to the Lexer file.

--- Lexer.cpp
+++ Lexer.cpp
@@ -74,6 +74,67 @@ static int gettok() {
     return ThisChar;
 }
 
+//===----------------------------------------------------------------------===//
+// Abstract Syntax Tree (aka Parse Tree)
+//===----------------------------------------------------------------------===//
+namespace {
+    /// ExprAST - Base class for all expression nodes.
+    class ExprAST {
+        public:
+            virtual ~ExprAST() {}
+    };
+
+    /// NumberExprAST - Expression class for numeric literals like "1.0".
+    class NumberExprAST : public ExprAST {
+        public:
+            NumberExprAST(double val) {}
+    };
+
+    /// VariableExprAST - Expression class for referencing a variable, like "a".
+    class VariableExprAST : public ExprAST {
+        std::string Name;
+        public:
+        VariableExprAST(const std::string &name) : Name(name) {}
+    };
+
+    /// BinaryExprAST - Expression class for a binary operator.
+    class BinaryExprAST : public ExprAST {
+        public:
+            BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs) {}
+    };
+
+    /// CallExprAST - Expression class for function calls.
+    class CallExprAST : public ExprAST {
+        std::string Callee;
+        std::vector<ExprAST*> Args;
+        public:
+        CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+            : Callee(callee), Args(args) {}
+    };
+
+    /// PrototypeAST - This class represents the "prototype" for a function,
+    /// which captures its name, and its argument names (thus implicitly the number
+    /// of arguments the function takes).
+    class PrototypeAST {
+        std::string Name;
+        std::vector<std::string> Args;
+        public:
+        PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+            : Name(name), Args(args) {}
+
+    };
+
+    /// FunctionAST - This class represents a function definition itself.
+    class FunctionAST {
+        public:
+            FunctionAST(PrototypeAST *proto, ExprAST *body) {}
+    };
+} // end anonymous namespace
+
+//===----------------------------------------------------------------------===//
+// Parser
+//===----------------------------------------------------------------------===//
+
 /// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
 /// token the parser is looking at.  getNextToken reads another token from the
 /// lexer and updates CurTok with its results.
@@ -81,64 +142,6 @@ static int CurTok;
 static int getNextToken() {
     return CurTok = gettok();
 }
-
-/// ExprAST - Base class for all expression nodes.
-class ExprAST {
-public:
-    virtual ~ExprAST() {}
-};
-
-/// NumberExprAST - Expression class for numeric literals like "1.0".
-class NumberExprAST : public ExprAST {
-    double Val;
-public:
-    NumberExprAST(double val) : Val(val) {}
-};
-
-/// VariableExprAST - Expression class for referencing a variable, like "a".
-class VariableExprAST : public ExprAST {
-    std::string Name;
-public:
-    VariableExprAST(const std::string &name) : Name(name) {}
-};
-
-/// BinaryExprAST - Expression class for a binary operator.
-class BinaryExprAST : public ExprAST {
-    char Op;
-    ExprAST *LHS, *RHS;
-public:
-    BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
-        : Op(op), LHS(lhs), RHS(rhs) {}
-};
-
-/// CallExprAST - Expression class for function calls.
-class CallExprAST : public ExprAST {
-    std::string Callee;
-    std::vector<ExprAST*> Args;
-public:
-    CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
-        : Callee(callee), Args(args) {}
-};
-
-/// PrototypeAST - This class represents the "prototype" for a function,
-/// which captures its name, and its argument names (thus implicitly the number
-/// of arguments the function takes).
-class PrototypeAST {
-    std::string Name;
-    std::vector<std::string> Args;
-public:
-    PrototypeAST(const std::string &name, const std::vector<std::string> &args)
-        : Name(name), Args(args) {}
-};
-
-/// FunctionAST - This class represents a function definition itself.
-class FunctionAST {
-    PrototypeAST *Proto;
-    ExprAST *Body;
-public:
-    FunctionAST(PrototypeAST *proto, ExprAST *body)
-        : Proto(proto), Body(body) {}
-};
 
 /// Error* - These are little helper functions for error handling.
 ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
