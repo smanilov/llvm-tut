d3ae26b4eb15f8e512daecd2d9c25ca7976138dc
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-13 20:10:02 +0000

    Start implementing ParseBinOpRHS
    The precedence value passed into ParseBinOpRHS indicates the minimal
    operator precedence that the function is allowed to eat. For example, if
    the current pair stream is [+, x] and ParseBinOpRHS is passed in a
    precedence of 40, it will not consume any tokens (because the precedence
    of ‘+’ is only 20). With this in mind, ParseBinOpRHS starts with:
    
    [code]
    
    This code gets the precedence of the current token and checks to see if
    if is too low. Because we defined invalid tokens to have a precedence of
    -1, this check implicitly knows that the pair-stream ends when the token
    stream runs out of binary operators.

--- Lexer.cpp
+++ Lexer.cpp
@@ -220,3 +220,17 @@ static ExprAST *ParseExpression() {
 
     return ParseBinOpRHS(0, LHS);
 }
+
+/// binoprhs
+///   ::= ('+' primary)*
+static ExprAST *ParseBinOpRHS(int ExprPrec, ExprAST *LHS) {
+    // If this is a binop, find its precedence.
+    while (1) {
+        int TokPrec = GetTokPrecedence();
+
+        // If this is a binop that binds at least as tightly as the current binop,
+        // consume it, otherwise we are done.
+        if (TokPrec < ExprPrec)
+            return LHS;
+    }
+}
