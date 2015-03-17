a45dfc7d0676a8a481c3f2b4f0317f41122bc46e
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-17 18:04:32 +0000

    Change enum class to enum - build succeeds

--- toy.cpp
+++ toy.cpp
@@ -13,7 +13,7 @@ using std::string;
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
-enum class Token {
+enum Token {
     tok_eof = -1,
 
     // commands
