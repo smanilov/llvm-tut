0affc2dfdb10daaa5b1f1295812a009d930426bb
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-19 23:32:21 +0000

    Move codegen code to a dedicated section
    The codegen functions of the AST classes are grouped together in a
    separate section of the code.

--- toy.cpp
+++ toy.cpp
@@ -108,10 +108,6 @@ namespace {
             virtual Value *Codegen();
     };
 
-    Value *NumberExprAST::Codegen() {
-        return ConstantFP::get(getGlobalContext(), APFloat(Val));
-    }
-
     /// VariableExprAST - Expression class for referencing a variable, like "a".
     class VariableExprAST : public ExprAST {
             string Name;
@@ -119,12 +115,6 @@ namespace {
             VariableExprAST(const string &name) : Name(name) {}
             virtual Value *Codegen();
     };
-
-    Value *VariableExprAST::Codegen() {
-        // Look this variable up in the function.
-        Value *V = NamedValues[Name];
-        return V ? V : ErrorV("Unknown variable name");
-    }
 
     /// BinaryExprAST - Expression class for a binary operator.
     class BinaryExprAST : public ExprAST {
@@ -136,24 +126,6 @@ namespace {
             virtual Value *Codegen() = 0;
     };
 
-    Value *BinaryExprAST::Codegen() {
-        Value *L = LHS->Codegen();
-        Value *R = RHS->Codegen();
-        if (L == 0 || R == 0) return 0;
-
-        switch (Op) {
-            case '+': return Builder.CreateFAdd(L, R, "addtmp");
-            case '-': return Builder.CreateFSub(L, R, "subtmp");
-            case '*': return Builder.CreateFMul(L, R, "multmp");
-            case '<':
-                L = Builder.CreateFCmpULT(L, R, "cmptmp");
-                // Convert bool 0/1 to double 0.0 or 1.0
-                return Builder.CreateUIToFP(L, Type::getDoubleTy(getGlobalContext()),
-                                            "booltmp");
-            default: return ErrorV("invalid binary operator");
-        }
-    }
-
     /// CallExprAST - Expression class for function calls.
     class CallExprAST : public ExprAST {
             string Callee;
@@ -163,25 +135,6 @@ namespace {
                 : Callee(callee), Args(args) {}
             virtual Value *Codegen() = 0;
     };
-
-    Value *CallExprAST::Codegen() {
-        // Look up the name in the global module table.
-        Function *CalleeF = TheModule->getFunction(Callee);
-        if (CalleeF == 0)
-            return ErrorV("Unknown function referenced");
-
-        // If argument mismatch error.
-        if (CalleeF->arg_size() != Args.size())
-            return ErrorV("Incorrect # arguments passed");
-
-        vector<Value*> ArgsV;
-        for (unsigned i = 0, e = Args.size(); i != e; ++i) {
-            ArgsV.push_back(Args[i]->Codegen());
-            if (ArgsV.back() == 0) return 0;
-        }
-
-        return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
-    }
 
     /// PrototypeAST - This class represents the "prototype" for a function,
     /// which captures its name, and its argument names (thus implicitly the number
@@ -195,50 +148,6 @@ namespace {
             virtual Value *Codegen() = 0;
     };
 
-    Function *PrototypeAST::Codegen() {
-        // Make the function type: double(double,double) etc.
-        vector<Type*> Doubles(Args.size(),
-                              Type::getDoubleTy(getGlobalContext()));
-        FunctionType *FT = FunctionType::get(Type::getDoubleTy(getGlobalContext()),
-                                             Doubles, false);
-
-        Function *F = Function::Create(FT, Function::ExternalLinkage, Name, TheModule);
-
-        // If F conflicted, there was already something named 'Name'. Thus the
-        // name of F would implicitly be changed to produce correct output. Use
-        // this to determine whether there was already something named 'Name'.
-        // If it has a body, don't allow redefinition or reextern.
-        if (F->getName() != Name) {
-            // Delete the one we just made and get the existing one.
-            F->eraseFromParent();
-            F = TheModule->getFunction(Name);
-
-            // If F already has a body, reject this.
-            if (!F->empty()) {
-                ErrorF("redefinition of function");
-                return 0;
-            }
-
-            // If F took a different number of args, reject.
-            if (F->arg_size() != Args.size()) {
-                ErrorF("redefinition of function with different # args");
-                return 0;
-            }
-        }
-
-        // Set names for all arguments.
-        unsigned Idx = 0;
-        for (Function::arg_iterator AI = F->arg_begin(); Idx != Args.size();
-             ++AI, ++Idx) {
-            AI->setName(Args[Idx]);
-
-            // Add arguments to variable symbol table.
-            NamedValues[Args[Idx]] = AI;
-        }
-
-        return F;
-    }
-
     /// FunctionAST - This class represents a function definition itself.
     class FunctionAST {
             PrototypeAST *Proto;
@@ -248,31 +157,6 @@ namespace {
                 : Proto(proto), Body(body) {}
             virtual Value *Codegen() = 0;
     };
-
-    Function *FunctionAST::Codegen() {
-        NamedValues.clear();
-
-        Function *TheFunction = Proto->Codegen();
-        if (TheFunction == 0)
-            return 0;
-
-        // Create a new basic block to start insertion into.
-        BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", TheFunction);
-        Builder.SetInsertPoint(BB);
-
-        if (Value *RetVal = Body->Codegen()) {
-            // Finish off the function.
-            Builder.CreateRet(RetVal);
-
-            // Validate the generated code, checking for consistency.
-            verifyFunction(*TheFunction);
-
-            return TheFunction;
-        }
-        // Error reading body, remove function
-        TheFunction->eraseFromParent();
-        return 0;
-    }
 } // end anonymous namespace
 
 //===----------------------------------------------------------------------===//
@@ -307,10 +191,6 @@ ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
 PrototypeAST *ErrorP(const char *Str) { Error(Str); return 0; }
 FunctionAST *ErrorF(const char *Str) { Error(Str); return 0; }
 Value *ErrorV(const char *Str) { Error(Str); return 0; }
-
-static Module *TheModule;
-static IRBuilder<> Builder(getGlobalContext());
-static map<string, Value*> NamedValues;
 
 static ExprAST *ParseExpression();
 
@@ -473,6 +353,130 @@ static FunctionAST *ParseTopLevelExpr() {
 static PrototypeAST *ParseExtern() {
     getNextToken();  // eat extern.
     return ParsePrototype();
+}
+
+//===----------------------------------------------------------------------===//
+// Code Generation 
+//===----------------------------------------------------------------------===//
+
+static Module *TheModule;
+static IRBuilder<> Builder(getGlobalContext());
+static map<string, Value*> NamedValues;
+
+Value *NumberExprAST::Codegen() {
+    return ConstantFP::get(getGlobalContext(), APFloat(Val));
+}
+
+Value *VariableExprAST::Codegen() {
+    // Look this variable up in the function.
+    Value *V = NamedValues[Name];
+    return V ? V : ErrorV("Unknown variable name");
+}
+
+Value *BinaryExprAST::Codegen() {
+    Value *L = LHS->Codegen();
+    Value *R = RHS->Codegen();
+    if (L == 0 || R == 0) return 0;
+
+    switch (Op) {
+        case '+': return Builder.CreateFAdd(L, R, "addtmp");
+        case '-': return Builder.CreateFSub(L, R, "subtmp");
+        case '*': return Builder.CreateFMul(L, R, "multmp");
+        case '<':
+            L = Builder.CreateFCmpULT(L, R, "cmptmp");
+            // Convert bool 0/1 to double 0.0 or 1.0
+            return Builder.CreateUIToFP(L, Type::getDoubleTy(getGlobalContext()),
+                                        "booltmp");
+        default: return ErrorV("invalid binary operator");
+    }
+}
+
+Value *CallExprAST::Codegen() {
+    // Look up the name in the global module table.
+    Function *CalleeF = TheModule->getFunction(Callee);
+    if (CalleeF == 0)
+        return ErrorV("Unknown function referenced");
+
+    // If argument mismatch error.
+    if (CalleeF->arg_size() != Args.size())
+        return ErrorV("Incorrect # arguments passed");
+
+    vector<Value*> ArgsV;
+    for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+        ArgsV.push_back(Args[i]->Codegen());
+        if (ArgsV.back() == 0) return 0;
+    }
+
+    return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+Function *PrototypeAST::Codegen() {
+    // Make the function type: double(double,double) etc.
+    vector<Type*> Doubles(Args.size(),
+                          Type::getDoubleTy(getGlobalContext()));
+    FunctionType *FT = FunctionType::get(Type::getDoubleTy(getGlobalContext()),
+                                         Doubles, false);
+
+    Function *F = Function::Create(FT, Function::ExternalLinkage, Name, TheModule);
+
+    // If F conflicted, there was already something named 'Name'. Thus the
+    // name of F would implicitly be changed to produce correct output. Use
+    // this to determine whether there was already something named 'Name'.
+    // If it has a body, don't allow redefinition or reextern.
+    if (F->getName() != Name) {
+        // Delete the one we just made and get the existing one.
+        F->eraseFromParent();
+        F = TheModule->getFunction(Name);
+
+        // If F already has a body, reject this.
+        if (!F->empty()) {
+            ErrorF("redefinition of function");
+            return 0;
+        }
+
+        // If F took a different number of args, reject.
+        if (F->arg_size() != Args.size()) {
+            ErrorF("redefinition of function with different # args");
+            return 0;
+        }
+    }
+
+    // Set names for all arguments.
+    unsigned Idx = 0;
+    for (Function::arg_iterator AI = F->arg_begin(); Idx != Args.size();
+         ++AI, ++Idx) {
+        AI->setName(Args[Idx]);
+
+        // Add arguments to variable symbol table.
+        NamedValues[Args[Idx]] = AI;
+    }
+
+    return F;
+}
+
+Function *FunctionAST::Codegen() {
+    NamedValues.clear();
+
+    Function *TheFunction = Proto->Codegen();
+    if (TheFunction == 0)
+        return 0;
+
+    // Create a new basic block to start insertion into.
+    BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", TheFunction);
+    Builder.SetInsertPoint(BB);
+
+    if (Value *RetVal = Body->Codegen()) {
+        // Finish off the function.
+        Builder.CreateRet(RetVal);
+
+        // Validate the generated code, checking for consistency.
+        verifyFunction(*TheFunction);
+
+        return TheFunction;
+    }
+    // Error reading body, remove function
+    TheFunction->eraseFromParent();
+    return 0;
 }
 
 //===----------------------------------------------------------------------===//
