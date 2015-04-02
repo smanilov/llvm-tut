a7ee8009aad94fadfd19c5db134593d746d205d2
Stan Manilov <stanislav.manilov@gmail.com>
2015-04-03 00:52:35 +0100

    Bugfix: switch default should be last
    Also, small comment fix.

--- toy.cpp
+++ toy.cpp
@@ -410,12 +410,12 @@ static ExprAST *ParseForExpr() {
 ///   ::= forexpr
 static ExprAST *ParsePrimary() {
     switch (CurTok) {
-        default: return Error("unknown token when expecting an expression");
         case tok_identifier: return ParseIdentifierExpr();
         case tok_number:     return ParseNumberExpr();
         case '(':            return ParseParenExpr();
         case tok_if:         return ParseIfExpr();
         case tok_for:        return ParseForExpr();
+        default: return Error("unknown token when expecting an expression");
     }
 }
 
@@ -464,7 +464,7 @@ static ExprAST *ParseExpression() {
 
 /// prototype
 ///   ::= id '(' id* ')'
-///   ::= binary LETTER number? (id, id)
+///   ::= binary LETTER number? (id id)
 static PrototypeAST *ParsePrototype() {
     string FnName;
 
